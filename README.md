5f7cca7a8eef9d84995b9c91dac150b481ee9930
Rodrigo <rodrigorael53@gmail.com>
2022-05-05 20:52:55 -0300

    creating repo challenges but only contract without implementations

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ entidades performam operações entre si, ou seja, os casos de uso. Somente com 
 é possível mapear o funcionamento da aplicação quase que por completo sem depender
 de banco de dados e sem depender de rotas ou qualquer coisa assim. Nos repositórios não é a implementação do repositório, em nenhum momento eu estou tocando em banco de dados, a partir dessa estrutura que estou criando pode ser utilizado qualquer fremework e qualquer banco de dados, eu estou criando aqui a abstração, os contratos, os contratos de conversação entre as camadas.
   - ex: o StudentsRepository é só um contrato, ele defini quais parâmetros minhas funções recebem, quais são essas funções e qual o retorno delas, ou seja, é só
-  o contrato, eu não tenho nada definido de implementação
+  o contrato, eu não tenho nada definido de implementação.
--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,5 @@
 import { Submission } from "../../domain/entities/submission";
+import { StudentsRepository } from "../repositories/StudentsRepository";
 
 type CreateChallengeSubmissionRequest = {
   studentId: string;
@@ -6,7 +7,22 @@ type CreateChallengeSubmissionRequest = {
 }
 
 export class CreateChallengeSubmission {
+  // eu não sei a implementação do repositório dentro do useCase mas eu sei o
+  // formato do repositório
+  constructor(
+    private studentsRepository: StudentsRepository,
+  ) {}
+
   async execute({studentId, challengeId}: CreateChallengeSubmissionRequest) {
+    // fazendo uma verificação
+    const student = this.studentsRepository.findById(studentId);
+
+    // se o estudante não existir
+    if (!student) {
+      // vai retornar um erro
+      throw new Error('Students does not exists.');
+    }
+
     const submission = Submission.create({
       studentId,
       challengeId
