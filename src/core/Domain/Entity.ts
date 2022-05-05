d7bcde99d78c29941b8311b21b321d779a497a35
Rodrigo <rodrigorael53@gmail.com>
2022-05-05 19:04:56 -0300

    creating a node-api with SOLID and TDD

---
+++ src/core/Domain/Entity.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto';
+
+// essa classe vai emplementer comportamentos padrãoes entre todas as entidades
+export abstract class Entity<T> {
+  // protected porque eu quero o id seja acessivel somente nessa classe e nas
+  // classes que estendem 
+  protected _id: string;
+  public props: T;
+  
+  constructor(props: T, id?: string) {
+    this.props = props;
+    this._id = id ?? crypto.randomUUID();
+  }
+}
